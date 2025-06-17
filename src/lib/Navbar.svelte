91b0664dc0042402f2c7650ab0da3324415affad
FROY MARTINEZ <f90ymtz@gmail.com>
2025-06-16 18:07:06 -0600

    First commit

---
+++ src/lib/Navbar.svelte
@@ -0,0 +1,9 @@
+<nav class="flex justify-between bg-orange-200 text-pink-950 p-2 boreder rounded-xs">
+    <a href="/"> Mi página web</a>
+
+    <ul class="flex gap-3">
+        <li>Acerca de mí</li>
+        <li>Portafolio</li>
+        <li>Contacto</li>
+    </ul>
+</nav>
