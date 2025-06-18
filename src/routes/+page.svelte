8f6be880d982141db8adcb14028838cc3178692b
FROY MARTINEZ <f90ymtz@gmail.com>
2025-06-18 16:49:47 -0600

    Hamburguesa

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,34 @@
 <script>
-    import Navbar from "$lib/Navbar.svelte";
+    let click = $state(false)  
+    function handleclick() {
+        click = !click    
+    }  
 </script>
 
-<Navbar /> 
+<nav class="flex justify-between items-center bg-orange-300 text-pink-900 p-2 boreder rounded-xs">
+    <a href="/"> Mi página web</a>
+
+    
+    <ul class="hidden md:flex gap-3">
+        <li>Acerca de mí</li>
+        <li>Portafolio</li>
+        <li>Contacto</li>
+    </ul>
+    <div class="flex md:hidden ">
+        <button onclick={handleclick}>
+            =
+        </button>
+    </div>
+</nav>
+
+<main>
+    <section>
+        {#if click}
+        <ul class="flex flex-col bg-orange-200 text-pink-800">
+            <li>Acerca de mí</li>
+            <li>Portafolio</li>
+            <li>Contacto</li>
+        </ul>
+        {/if}
+    </section>
+</main>
