c869590ed25294a6c1c3c7c3e97091009907d2c3
FROY MARTINEZ <f90ymtz@gmail.com>
2025-06-18 21:11:59 -0600

    Hamburger icon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
     }  
 </script>
 
-<nav class="flex justify-between items-center bg-orange-300 text-pink-900 p-2 boreder rounded-xs">
+<nav class="flex justify-between items-center bg-orange-300 text-red-800 p-2 boreder rounded-xs">
     <a href="/"> Mi página web</a>
 
     
@@ -16,7 +16,7 @@
     </ul>
     <div class="flex md:hidden ">
         <button onclick={handleclick}>
-            =
+            <img src="/Hamburguer_icon.png" alt="Botón ícono" class="w-8 h-8" />
         </button>
     </div>
 </nav>
@@ -24,7 +24,7 @@
 <main>
     <section>
         {#if click}
-        <ul class="flex flex-col bg-orange-200 text-pink-800">
+        <ul class="flex flex-col bg-orange-200 text-red-800 md:hidden">
             <li>Acerca de mí</li>
             <li>Portafolio</li>
             <li>Contacto</li>
